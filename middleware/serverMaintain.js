a210148f2b4426b4e03067fc24260c7134ce93d7
shahidx0x <shahid.dev.cse@gmail.com>
2023-10-05 18:11:47 +0600

    bug

--- middleware/serverMaintain.js
+++ middleware/serverMaintain.js
@@ -1,6 +1,8 @@
 const serverMaintain = (req, res, next) => {
   if (process.env.MAINTENANCE_MODE === "true") {
-    return res.status(503).send("Server is in maintenance");
+    return res
+      .status(503)
+      .json({ status: 503, message: "Server is in maintenance" });
   }
   next();
 };
