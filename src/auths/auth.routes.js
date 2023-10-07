7afd52e633f36e1962065595f1aacbe32ffb5bd2
shahidx0x <shahid.dev.cse@gmail.com>
2023-10-07 21:31:19 +0600

    added authenticate

--- src/auths/auth.routes.js
+++ src/auths/auth.routes.js
@@ -1,13 +1,19 @@
+const authenticate = require("../../middleware/authenticate");
 const { authLimiter } = require("../../middleware/rateLimiter");
 const authController = require("./auth.controller");
 const auth_router = require("express").Router();
 
 auth_router.get("/verify/email/:cypher", authController.verify_email);
-auth_router.get("/get/users", authController.getAllUsers);
-auth_router.get("/get/user", authController.getUserByEmail);
+auth_router.get("/get/users", authenticate, authController.getAllUsers);
+auth_router.get("/get/user", authenticate, authController.getUserByEmail);
 auth_router.post("/signup", authLimiter, authController.createUser);
 auth_router.post("/signin", authLimiter, authController.signInUser);
-auth_router.patch("/user/info/update", authLimiter, authController.updateUser);
+auth_router.patch(
+  "/user/info/update",
+  authenticate,
+  authLimiter,
+  authController.updateUser
+);
 auth_router.post(
   "/forgot/password/:email",
   authLimiter,
