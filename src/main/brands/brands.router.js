f66d52d9655285d76b50144a4a3e8ac8d38b61d6
shahidx0x <shahid.dev.cse@gmail.com>
2023-10-12 20:05:32 +0600

    bug fixed

--- src/main/brands/brands.router.js
+++ src/main/brands/brands.router.js
@@ -4,26 +4,31 @@ const brandsController = require("./brands.controller");
 const authenticate = require("../../../middleware/authenticate");
 
 brands_router.post(
+  authenticate,
   "/create/brands",
 
   brandsController.createBrand
 );
 brands_router.get(
+  authenticate,
   "/get/all/brands",
 
   brandsController.getAllBrands
 );
 brands_router.get(
+  authenticate,
   "/get/brands/by/:id",
 
   brandsController.getBrandById
 );
 brands_router.delete(
+  authenticate,
   "/delete/brands/by/:id",
 
   brandsController.deleteBrandById
 );
 brands_router.patch(
+  authenticate,
   "/update/brands/by/:id",
 
   brandsController.updateBrandById
