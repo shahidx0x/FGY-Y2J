0d92d0737b53e3f501dd538f02b6d8b4b01c730f
shahidx0x <shahid.dev.cse@gmail.com>
2023-10-30 18:59:17 +0600

    sub category added

--- src/main/categories/subcategories/subcategories.controller.js
+++ src/main/categories/subcategories/subcategories.controller.js
@@ -4,15 +4,45 @@ const SubCategoryController = {
   getAllSubCategories: async (req, res) => {
     try {
       const { categoryId } = req.params;
+      const page = parseInt(req.query.page, 10) || 1;
+      let limit = parseInt(req.query.limit, 10) || 10;
+
       const category = await Category.findById(categoryId);
+
       if (!category) {
         return res.status(404).json({ message: "Category not found" });
       }
-      res.status(200).json({ subCategories: category.subCategories });
+
+      let paginatedSubCategories;
+      let totalPages;
+
+      if (limit === -1) {
+        paginatedSubCategories = category.subCategories;
+        totalPages = 1;
+      } else {
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        paginatedSubCategories = category.subCategories.slice(
+          startIndex,
+          endIndex
+        );
+        totalPages = Math.ceil(category.subCategories.length / limit);
+      }
+
+      res.status(200).json({
+        meta: {
+          total_subcategories: category.subCategories.length,
+          total_page: totalPages,
+          current_page: page,
+          per_page: limit,
+        },
+        data: paginatedSubCategories,
+      });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   },
+
   getSubCategoryById: async (req, res) => {
     try {
       const { categoryId, subCategoryId } = req.params;
--- src/main/categories/subcategories/subcategories.router.js
+++ src/main/categories/subcategories/subcategories.router.js
@@ -2,6 +2,11 @@ const express = require("express");
 const SubCategoryController = require("./subcategories.controller");
 const subcat_router = express.Router();
 
+subcat_router.get(
+  "/get/all/subcat/:categoryId",
+  SubCategoryController.getAllSubCategories
+);
+
 subcat_router.post(
   "/create/subcategory/:categoryId",
   SubCategoryController.addSubCategory
