290977603c141b01c680d99bd8d627f1077c8a1a
shahidx0x <shahid.dev.cse@gmail.com>
2023-11-07 02:17:13 +0600

    server status added

--- src/main/dashbord/dashbord.controller.js
+++ src/main/dashbord/dashbord.controller.js
@@ -1,17 +1,44 @@
 const os = require("os");
-const { checkDiskSpace } = require("node-disk-info");
-module.exports = dashbordController = {
+const nodeDiskInfo = require("node-disk-info");
+
+module.exports = dashboardController = {
   server_status: async (req, res) => {
     try {
+      // CPU Information
       const cpus = os.cpus();
+      let totalIdleTime = 0;
+      let totalTickTime = 0;
+
+      cpus.forEach((core) => {
+        for (type in core.times) {
+          totalTickTime += core.times[type];
+        }
+        totalIdleTime += core.times.idle;
+      });
+
+      const totalUtilTime = totalTickTime - totalIdleTime;
+      const cpuUsagePercent = Math.round((100 * totalUtilTime) / totalTickTime);
+
+      // Memory Information
       const totalMemory = os.totalmem();
       const freeMemory = os.freemem();
-      const diskInfo = await checkDiskSpace("/");
+      const memoryUsagePercent = Math.round(
+        (100 * (totalMemory - freeMemory)) / totalMemory
+      );
+
+      // Disk Information
+      const diskInfo = await nodeDiskInfo.getDiskInfo();
+      const diskSpace = diskInfo.find((disk) => disk.mounted === "/");
+      // If the library provides used space directly, you should use it; otherwise, calculate it
+      const usedDiskSpace = diskSpace.blocks - diskSpace.available;
+      const diskUsagePercent = Math.round(
+        (100 * usedDiskSpace) / diskSpace.blocks
+      );
+
       res.json({
-        cpus,
-        totalMemory,
-        freeMemory,
-        diskInfo,
+        cpuUsage: cpuUsagePercent,
+        memoryUsage: memoryUsagePercent,
+        diskUsage: diskUsagePercent,
       });
     } catch (error) {
       res.status(500).json({ error: error.message });
--- src/main/dashbord/dashbord.router.js
+++ src/main/dashbord/dashbord.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const dashbordController = require("./dashbord.controller");
 const dashbord_router = express.Router();
-dashbord_router.get("/get/system/status",dashbordController.server_status);
+dashbord_router.get("/server-status", dashbordController.server_status);
 module.exports = dashbord_router;
