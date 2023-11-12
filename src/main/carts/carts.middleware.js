a65bcea2b5b701a4169a6df824dc143884d95163
shahidx0x <shahid.dev.cse@gmail.com>
2023-11-13 05:11:35 +0600

    updated middleware

--- src/main/carts/carts.middleware.js
+++ src/main/carts/carts.middleware.js
@@ -11,37 +11,35 @@ const updateCartItemDetails = async (req, res, next) => {
     }
 
     let isCartUpdated = false;
+    const updatedItems = [];
 
-    cart.items = await Promise.all(
-      cart.items.map(async (item) => {
-        const product = await Products.findById(item.product_id);
-        if (product) {
-          let itemUpdated = false;
-          if (product.price !== item.price) {
-            item.price = product.price;
-            itemUpdated = true;
-          }
-          if (product.product_image !== item.product_image) {
-            item.product_image = product.product_image;
-            itemUpdated = true;
-          }
-          if (product.name !== item.product_name) {
-            item.product_name = product.name;
-            itemUpdated = true;
-          }
-          if (itemUpdated) {
-            isCartUpdated = true;
-          }
-          return item;
+    for (const item of cart.items) {
+      const product = await Products.findById(item.product_id);
+      if (product) {
+        let itemUpdated = false;
+        if (product.price !== item.price) {
+          item.price = product.price;
+          itemUpdated = true;
         }
+        if (product.product_image !== item.product_image) {
+          item.product_image = product.product_image;
+          itemUpdated = true;
+        }
+        if (product.name !== item.product_name) {
+          item.product_name = product.name;
+          itemUpdated = true;
+        }
+        if (itemUpdated) {
+          isCartUpdated = true;
+        }
+        updatedItems.push(item);
+      } else {
+        isCartUpdated = true;
+      }
+    }
 
-        return null;
-      })
-    );
-
-    cart.items = cart.items.filter((item) => item !== null);
-
-    if (isCartUpdated || cart.items.length !== cart.items.length) {
+    if (isCartUpdated) {
+      cart.items = updatedItems;
       await cart.save();
     }
 
@@ -50,4 +48,5 @@ const updateCartItemDetails = async (req, res, next) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 module.exports = updateCartItemDetails;
