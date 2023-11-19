88a4a9d03654f2207e0164b405c568e8acbd4bfe
shahidx0x <shahid.dev.cse@gmail.com>
2023-11-19 06:25:36 +0600

    models updated

--- src/main/products/products.model.js
+++ src/main/products/products.model.js
@@ -19,51 +19,54 @@ const skuSchema = new mongoose.Schema({
   stock: Number,
 });
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  isDisable: {
-    type: Boolean,
-    default: false,
+const productSchema = new mongoose.Schema(
+  {
+    name: String,
+    isDisable: {
+      type: Boolean,
+      default: false,
+    },
+    description: String,
+    brand_id: String,
+    brand_name: String,
+    category_id: String,
+    category_name: String,
+    subcategory_id: String,
+    subcategory_name: String,
+    product_image: String,
+    discount: Number,
+    base_price: Number,
+    fet_image: [],
+    min_purchease: Number,
+    max_purchease: Number,
+    price: Number,
+    product_information: String,
+    varient: {
+      type: [varientSchema],
+      default: [
+        {
+          base_price: 0,
+          discount: 0,
+          price: 0,
+          min_purchease: 0,
+          max_purchease: 0,
+        },
+      ],
+    },
+    sku: {
+      type: [skuSchema],
+      default: [
+        {
+          booked: 0,
+          available: 0,
+          ongoing: 0,
+          stock: 0,
+        },
+      ],
+    },
   },
-  description: String,
-  brand_id: String,
-  brand_name: String,
-  category_id: String,
-  category_name: String,
-  subcategory_id: String,
-  subcategory_name: String,
-  product_image: String,
-  discount: Number,
-  base_price: Number,
-  fet_image: [],
-  min_purchease: Number,
-  max_purchease: Number,
-  price: Number,
-  product_information: String,
-  varient: {
-    type: [varientSchema],
-    default: [
-      {
-        base_price: 0,
-        discount: 0,
-        price: 0,
-        min_purchease: 0,
-        max_purchease: 0,
-      },
-    ],
-  },
-  sku: {
-    type: [skuSchema],
-    default: [
-      {
-        booked: 0,
-        available: 0,
-        ongoing: 0,
-        stock: 0,
-      },
-    ],
-  },
-});
+  { timestamps: true }
+);
 productSchema.pre("save", function (next) {
   const product = this;
   if (product.isNew) {
--- src/main/transaction/transaction.model.js
+++ src/main/transaction/transaction.model.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
-const transactionSchema = new mongoose.Schema({
-  user: {
-    type: String,
+const transactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    items: [],
+    amount: {
+      type: Number,
+      required: true,
+    },
+    deliveryStatus: {
+      type: String,
+    },
+    purchaseStatus: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  items: [],
-  amount: {
-    type: Number,
-    required: true,
-  },
-  deliveryStatus: {
-    type: String,
-  },
-  purchaseStatus: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 module.exports = Transaction;
