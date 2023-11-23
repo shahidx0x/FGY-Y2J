4de701147aeacf02bf6a1b38413a66837c894115
shahidx0x <shahid.dev.cse@gmail.com>
2023-11-23 14:00:22 +0600

    bug fixed

--- src/main/notification/notification.router.js
+++ src/main/notification/notification.router.js
@@ -5,7 +5,6 @@ const notification_router = express.Router();
 
 notification_router.get(
   "/notifications",
-  authenticate,
   notification_controller.getAllNotifications
 );
 notification_router.post(
@@ -14,7 +13,6 @@ notification_router.post(
 );
 notification_router.patch(
   "/notifications",
-  authenticate,
   notification_controller.updateNotificationById
 );
 module.exports = notification_router;
