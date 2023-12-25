2481ba75c354a7415eca4da5e073b31f84e7d4a7
shahidx0x <dev.imshahid@gmail.com>
2023-12-25 15:36:43 +0600

    update carts.controller.js

--- src/main/carts/carts.controller.js
+++ src/main/carts/carts.controller.js
@@ -6,23 +6,44 @@ const cartController = {
     try {
       const { product_id, quantity } = req.body;
       const user_email = req.userEmail;
+      let cart = await Cart.findOne({ user_email });
+
+      if (!cart) {
+        cart = new Cart({
+          user_email,
+          items: [],
+        });
+      }
+
+      const isUpdating = cart.isUpdating;
+      if (isUpdating) {
+        return res
+          .status(409)
+          .json({ message: "Please wait for the update to complete." });
+      }
+
+      cart.isUpdating = true;
       let product = await Products.findById(product_id);
+
       if (!product) {
+        cart.isUpdating = false;
         return res.status(404).json({ message: "Product not found" });
       }
-      if (product) {
-        if (product.isDisable) {
-          return res
-            .status(200)
-            .json({ message: "Product is not available right now" });
-        }
+
+      if (product.isDisable) {
+        cart.isUpdating = false;
+        return res
+          .status(200)
+          .json({ message: "Product is not available right now" });
       }
+
       if (product.discount > 0) {
         product.afterDiscount =
-          product.price - (product.price * product.discount) / 100;
-      } else if (product.discount === 0 || product.discount < 0) {
+          (product.price * (100 - product.discount)) / 100;
+      } else {
         product.afterDiscount = product.price;
       }
+
       const {
         name,
         price,
@@ -33,57 +54,32 @@ const cartController = {
         product_unit_quantity,
         unit_flag,
       } = product;
-      let cart = await Cart.findOne({ user_email });
-      if (!cart) {
-        cart = new Cart({
-          user_email,
-          items: [
-            {
-              product_name: name,
-              product_image,
-              product_id,
-              product_unit_type,
-              product_unit: product_unit_type + "/" + product_unit_quantity,
-              product_unit_value: product_unit_quantity,
-              unit_flag,
-              quantity,
-              price,
-              afterDiscount,
-              discount,
-            },
-          ],
-        });
-      } else {
-        if (cart.isUpdating) {
-          return res.status(409).json({ message: "Please Wait ..." });
-        }
-        cart.isUpdating = true;
-        const itemIndex = cart.items.findIndex(
-          (item) =>
-            item.product_id._id.toString() ===
-            new mongoose.Types.ObjectId(product_id).toString()
-        );
 
-        if (itemIndex > -1) {
-          cart.items[itemIndex].quantity += quantity;
-        } else {
-          cart.items.push({
-            product_name: name,
-            product_image,
-            product_id,
-            quantity,
-            product_unit_type,
-            product_unit: product_unit_type + "/" + product_unit_quantity,
-            product_unit_value: product_unit_quantity,
-            unit_flag,
-            price,
-            afterDiscount,
-            discount,
-          });
-        }
-        cart.isUpdating = false;
+      const itemIndex = cart.items.findIndex(
+        (item) => item.product_id.toString() === product_id.toString()
+      );
+
+      if (itemIndex > -1) {
+        cart.items[itemIndex].quantity += quantity;
+      } else {
+        cart.items.push({
+          product_name: name,
+          product_image,
+          product_id,
+          quantity,
+          product_unit_type,
+          product_unit: `${product_unit_type}/${product_unit_quantity}`,
+          product_unit_value: product_unit_quantity,
+          unit_flag,
+          price,
+          afterDiscount,
+          discount,
+        });
       }
+
+      cart.isUpdating = false;
       await cart.save();
+
       res.status(201).json(cart);
     } catch (error) {
       res.status(500).json({ message: error.message });
