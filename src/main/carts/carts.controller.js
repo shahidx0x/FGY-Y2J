a346c6382868195f309fed71edbf0ce9402e33e6
shahidx0x <dev.imshahid@gmail.com>
2023-12-25 19:19:21 +0600

    Revert "Update carts.controller.js"
    This reverts commit 7abe05e58d4bdef8e9506cc862da56f203a56c7e.

--- src/main/carts/carts.controller.js
+++ src/main/carts/carts.controller.js
@@ -2,83 +2,111 @@ const Cart = require("./carts.model");
 const Products = require("../products/products.model");
 const mongoose = require("mongoose");
 const cartController = {
-  
-addItem: async (req, res) => {
-  try {
-    const { product_id, quantity } = req.body;
-    const user_email = req.userEmail;
+  addItem: async (req, res) => {
+    try {
+      const { product_id, quantity } = req.body;
+      const user_email = req.userEmail;
 
-    const cart = await Cart.findOneAndUpdate(
-      { user_email, isUpdating: false },
-      { $set: { isUpdating: true } },
-      { upsert: true, new: true }
-    );
+      let cart = await Cart.findOne({ user_email });
 
-    if (!cart) {
-      return res.status(409).json({ message: "Please wait for the update to complete." });
-    }
+      if (!cart) {
+        cart = new Cart({
+          user_email,
+          items: [],
+        });
+      }
 
-    let product = await Products.findById(product_id);
+      if (cart.isUpdating) {
+        return res
+          .status(409)
+          .json({ message: "Please wait for the update to complete." });
+      }
 
-    if (!product) {
-      cart.isUpdating = false;
+      cart.isUpdating = true;
       await cart.save();
-      return res.status(404).json({ message: "Product not found" });
-    }
 
-    if (product.isDisable) {
-      cart.isUpdating = false;
-      await cart.save();
-      return res.status(200).json({ message: "Product is not available right now" });
-    }
+      let product = await Products.findById(product_id);
 
-    if (product.discount > 0) {
-      product.afterDiscount = (product.price * (100 - product.discount)) / 100;
-    } else {
-      product.afterDiscount = product.price;
-    }
+      if (!product) {
+        cart.isUpdating = false;
+        return res.status(404).json({ message: "Product not found" });
+      }
 
-    const {
-      name,
-      price,
-      afterDiscount,
-      discount,
-      product_image,
-      product_unit_type,
-      product_unit_quantity,
-      unit_flag,
-    } = product;
+      if (product.isDisable) {
+        cart.isUpdating = false;
+        return res
+          .status(200)
+          .json({ message: "Product is not available right now" });
+      }
 
-    const itemIndex = cart.items.findIndex(
-      (item) => item.product_id.toString() === product_id.toString()
-    );
+      if (product.discount > 0) {
+        product.afterDiscount =
+          (product.price * (100 - product.discount)) / 100;
+      } else {
+        product.afterDiscount = product.price;
+      }
 
-    if (itemIndex > -1) {
-      cart.items[itemIndex].quantity += quantity;
-    } else {
-      cart.items.push({
-        product_name: name,
-        product_image,
-        product_id,
-        quantity,
-        product_unit_type,
-        product_unit: `${product_unit_type}/${product_unit_quantity}`,
-        product_unit_value: product_unit_quantity,
-        unit_flag,
+      const {
+        name,
         price,
         afterDiscount,
         discount,
-      });
+        product_image,
+        product_unit_type,
+        product_unit_quantity,
+        unit_flag,
+      } = product;
+
+      const itemIndex = cart.items.findIndex(
+        (item) => item.product_id.toString() === product_id.toString()
+      );
+
+      if (itemIndex > -1) {
+        cart.items[itemIndex].quantity += quantity;
+      } else {
+        cart.items.push({
+          product_name: name,
+          product_image,
+          product_id,
+          quantity,
+          product_unit_type,
+          product_unit: `${product_unit_type}/${product_unit_quantity}`,
+          product_unit_value: product_unit_quantity,
+          unit_flag,
+          price,
+          afterDiscount,
+          discount,
+        });
+      }
+
+      cart.isUpdating = false;
+      await cart.save();
+
+      res.status(201).json(cart);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: error.message });
     }
+  },
 
-    cart.isUpdating = false;
-    await cart.save();
+  removeItem: async (req, res) => {
+    try {
+      const { user_email, product_id } = req.body;
 
-    res.status(201).json(cart);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-},
+      const cart = await Cart.findOne({ user_email });
+      if (!cart) {
+        return res.status(200).json({ message: "Cart is empty" });
+      }
+      cart.items = cart.items.filter(
+        (item) => item.product_id.toString() !== product_id
+      );
+
+      await cart.save();
+      res.status(200).json(cart);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
 
   getUserCart: async (req, res) => {
     try {
