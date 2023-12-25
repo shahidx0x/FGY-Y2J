a1ddb241ccb1f47b19c51869cbc4c49d5ee790b8
shahidx0x <dev.imshahid@gmail.com>
2023-12-26 03:08:57 +0600

    update carts.controller.js

--- src/main/carts/carts.controller.js
+++ src/main/carts/carts.controller.js
@@ -2,97 +2,29 @@ const Cart = require("./carts.model");
 const Products = require("../products/products.model");
 const mongoose = require("mongoose");
 const cartController = {
-  // addItem: async (req, res) => {
-  //   try {
-  //     const { product_id, quantity } = req.body;
-  //     const user_email = req.userEmail;
-  //     let product = await Products.findById(product_id);
-
-  //     if (!product) {
-  //       return res.status(404).json({ message: "Product not found" });
-  //     }
-
-  //     if (product.isDisable) {
-  //       return res
-  //         .status(200)
-  //         .json({ message: "Product is not available right now" });
-  //     }
-
-  //     if (product.discount > 0) {
-  //       product.afterDiscount =
-  //         product.price - (product.price * product.discount) / 100;
-  //     } else if (product.discount === 0 || product.discount < 0) {
-  //       product.afterDiscount = product.price;
-  //     }
-
-  //     const {
-  //       name,
-  //       price,
-  //       afterDiscount,
-  //       discount,
-  //       product_image,
-  //       product_unit_type,
-  //       product_unit_quantity,
-  //       unit_flag,
-  //     } = product;
-
-  //     let cart = await Cart.findOneAndUpdate(
-  //       { user_email, "items.product_id": product_id },
-  //       {
-  //         $inc: { "items.$.quantity": quantity },
-  //       },
-  //       { new: true }
-  //     );
-
-  //     if (!cart) {
-  //       cart = await Cart.findOneAndUpdate(
-  //         { user_email },
-  //         {
-  //           $push: {
-  //             items: {
-  //               product_name: name,
-  //               product_image,
-  //               product_id,
-  //               product_unit_type,
-  //               product_unit: product_unit_type + "/" + product_unit_quantity,
-  //               product_unit_value: product_unit_quantity,
-  //               unit_flag,
-  //               quantity,
-  //               price,
-  //               afterDiscount,
-  //               discount,
-  //             },
-  //           },
-  //         },
-  //         { new: true, upsert: true }
-  //       );
-  //     }
-
-  //     res.status(201).json(cart);
-  //   } catch (error) {
-  //     res.status(500).json({ message: error.message });
-  //   }
-  // },
   addItem: async (req, res) => {
     try {
       const { product_id, quantity } = req.body;
       const user_email = req.userEmail;
       let product = await Products.findById(product_id);
-  
+
       if (!product) {
         return res.status(404).json({ message: "Product not found" });
       }
-  
+
       if (product.isDisable) {
-        return res.status(200).json({ message: "Product is not available right now" });
+        return res
+          .status(200)
+          .json({ message: "Product is not available right now" });
       }
-  
+
       if (product.discount > 0) {
-        product.afterDiscount = product.price - (product.price * product.discount) / 100;
+        product.afterDiscount =
+          product.price - (product.price * product.discount) / 100;
       } else if (product.discount === 0 || product.discount < 0) {
         product.afterDiscount = product.price;
       }
-  
+
       const {
         name,
         price,
@@ -103,42 +35,45 @@ const cartController = {
         product_unit_quantity,
         unit_flag,
       } = product;
-  
-      let cart = await Cart.findOne({ user_email });
-  
+
+      let cart = await Cart.findOneAndUpdate(
+        { user_email, "items.product_id": product_id },
+        {
+          $inc: { "items.$.quantity": quantity },
+        },
+        { new: true }
+      );
+
       if (!cart) {
-        // If the product is not already in the cart, add a new item
-        cart = new Cart({
-          user_email,
-          items: [
-            {
-              product_name: name,
-              product_image,
-              product_id,
-              product_unit_type,
-              product_unit: product_unit_type + "/" + product_unit_quantity,
-              product_unit_value: product_unit_quantity,
-              unit_flag,
-              quantity,
-              price,
-              afterDiscount,
-              discount,
+        cart = await Cart.findOneAndUpdate(
+          { user_email },
+          {
+            $push: {
+              items: {
+                product_name: name,
+                product_image,
+                product_id,
+                product_unit_type,
+                product_unit: product_unit_type + "/" + product_unit_quantity,
+                product_unit_value: product_unit_quantity,
+                unit_flag,
+                quantity,
+                price,
+                afterDiscount,
+                discount,
+              },
             },
-          ],
-        });
-      } else {
-        // If the product is already in the cart, update the quantity
-        await Cart.updateOne(
-          { user_email, 'items.product_id': product_id },
-          { $inc: { 'items.$.quantity': quantity } }
+          },
+          { new: true, upsert: true }
         );
       }
-  
+
       res.status(201).json(cart);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   },
+
   
   removeItem: async (req, res) => {
     try {
